0e599f48ab0b98c61478eb4ac0816afd0e2e5b98
Carlos <126183973+ccalvop@users.noreply.github.com>
2023-03-17 11:29:29 +0100

    Add files via upload

---
+++ resize_EBS/resize_EBS.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Declare variables
+INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+VOLUME_ID=$(aws ec2 describe-instances \
+  --instance-id $INSTANCE_ID \
+  --query "Reservations[0].Instances[0].BlockDeviceMappings[0].Ebs.VolumeId" \
+  --output text)
+
+# Specify the desired volume size in GiB as a command-line argument. If not specified, default to 20 GiB.
+SIZE=${1:-20}
+
+# Resize the EBS volume.
+aws ec2 modify-volume --volume-id $VOLUME_ID --size $SIZE
+
+# Wait for the resize to finish.
+while [ \
+  "$(aws ec2 describe-volumes-modifications \
+    --volume-id $VOLUME_ID \
+    --filters Name=modification-state,Values="optimizing","completed" \
+    --query "length(VolumesModifications)"\
+    --output text)" != "1" ]; do
+sleep 1
+done
+
+#Check if we're on an NVMe filesystem
+if [ $(readlink -f /dev/xvda) = "/dev/xvda" ]
+then
+  # Rewrite the partition table so that the partition takes up all the space that it can.
+  sudo growpart /dev/xvda 1
+
+  # Expand the size of the file system.
+  # Check if we are on AL2
+  STR=$(cat /etc/os-release)
+  SUB="VERSION_ID=\"2\""
+  if [[ "$STR" == *"$SUB"* ]]
+  then
+    sudo xfs_growfs -d /
+  else
+    sudo resize2fs /dev/xvda1
+  fi
+
+else
+  # Rewrite the partition table so that the partition takes up all the space that it can.
+  sudo growpart /dev/nvme0n1 1
+
+  # Expand the size of the file system.
+  # Check if we're on AL2
+  STR=$(cat /etc/os-release)
+  SUB="VERSION_ID=\"2\""
+  if [[ "$STR" == *"$SUB"* ]]
+  then
+    sudo xfs_growfs -d /
+  else
+    sudo resize2fs /dev/nvme0n1p1
+  fi
+fi
